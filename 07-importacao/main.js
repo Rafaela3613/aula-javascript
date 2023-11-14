d84b1077a16addd73c67ed41ba32a70954553e47
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-11-14 15:33:20 -0300

    aplicando funcoes auxiliares

--- 07-importacao/main.js
+++ 07-importacao/main.js
@@ -1,3 +1,19 @@
-const funcoes = require ('./funcoes-auxiliares');
+const {gets, print} = require ('./funcoes-auxiliares');
 
-console.log(funcoes.gets());
+const numerosSorteados = []; 
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteados = gets();
+    numerosSorteados.push(numeroSorteados);
+        
+}
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteados = numerosSorteados[i];
+    if(numeroSorteados>maiorValor){
+        maiorValor = numeroSorteados;
+    }
+}
+
+print(maiorValor);
